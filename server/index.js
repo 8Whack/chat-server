3e7fc697e2eeb22eb8d734492a0077d5f6493ae5
8Whack <calvinpratt99@gmail.com>
2022-03-14 10:27:23 -0700

    added controller features to the index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const controller = require('./controllers/messages_controller');
+
 const app = express();
 
 app.use(express.json());
+
+const url = '/api/messages';
+app.get(url, controller.read);
+app.post(url, controller.create);
+app.put(`${url}/:id`, controller.update);
+app.delete(`${url}/:id`, controller.delete);
+
+
 
 const port = 3001;
 app.listen(port, ()=>{
